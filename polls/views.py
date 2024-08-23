28395ed8c417078453ed7f2cb555c690fd00fc4d
usmon <usmonkarimov0726@gmail.com>
2024-08-23 22:27:40 +0500

    Edtinig views

--- polls/views.py
+++ polls/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(reques):
-    return HttpResponse ("Hello, wolrd . You are at the polls index")
+    return HttpResponse ("Hello, wolrd . You are at the olls index")
