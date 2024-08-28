0149e302292148d64dbc86c696580b17cd434e73
usmon <usmonkarimov0726@gmail.com>
2024-08-28 09:36:01 +0500

    Working on views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
 from . import views
-
+app_name = "polls"
 urlpatterns = [
-    path('',views.index, name = 'index')
+    path('',views.index, name = 'index'),
+    path("<int:question_id>/", views.detail ,name = 'detail-page'),
+    path("<int:question_id>/result/", views.results, name = "result-page"),
+    path("<int:question_id>/vote/", views.vote, name="vote-page")
+    
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,Http404
+from .models import Question
 # Create your views here.
 
-def index(reques):
-    return HttpResponse ("Hello, wolrd . You are at the olls index")
+def index(request):
+    latest_questions = Question.objects.order_by("-pub_date")[:5]
+    response= render(request,"polls/index.html",{
+        "questions":latest_questions
+    })
+    return response
+    
+def detail(request,question_id):
+    question = get_object_or_404(Question,pk = question_id)
+    return render(request,'polls/detail.html',{"question" : question})
+    
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
